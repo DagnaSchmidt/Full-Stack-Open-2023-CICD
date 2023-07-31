41a4ba2f12c683c40b3e313b9177dc518cfafa41
Dagna Schmidt <dagdagi889@gmail.com>
2023-07-31 11:12:31 +0200

    git fix

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,5 @@ import App from './App'
 import './styles.css'
 
 const container = document.getElementById('app')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const root = createRoot(container)
 root.render(<App />)
